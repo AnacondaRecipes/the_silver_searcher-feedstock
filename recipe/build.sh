7ccd91d4e2b96ed56eb33b103605834479493395
Ray Donnelly <mingw.android@gmail.com>
2017-11-22 21:20:16 +0000

    Update to 2.1.0, add Windows and lots of new features

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,22 @@
 #!/usr/bin/env bash
 
-export LDFLAGS="$LDFLAGS -Wl,-rpath,${PREFIX}/lib -L${PREFIX}/lib -lz"
+if [[ $(uname) == MINGW* ]]; then
+  if [[ ${ARCH} == 32 ]]; then
+    HOST_BUILD="--host=i686-w64-mingw32 --build=i686-w64-mingw32"
+  else
+    HOST_BUILD="--host=x86_64-w64-mingw32 --build=x86_64-w64-mingw32"
+  fi
+  PREFIX="${PREFIX}"/Library/mingw-w64
+  export PKG_CONFIG_PATH="${PREFIX}"/lib/pkgconfig
+else
+  export HOST_BUILD="--host=${HOST}"
+fi
+
+if [[ ${HOST} =~ .*darwin.* ]]; then
+  export LDFLAGS=${LDFLAGS_CC}
+fi
+
 ./autogen.sh
-./configure --prefix="${PREFIX}"
-make
-make test
-make -j${CPU_COUNT} install
+./configure --prefix="${PREFIX}" ${HOST_BUILD}
+make -j${CPU_COUNT} ${VERBOSE_AT}
+make install
